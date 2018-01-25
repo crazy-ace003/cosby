a6a1b9ae8269823756b4aac5c3fa50dde8beb90e
Michael Meade <crazyace003@gmail.com>
2018-01-25 18:57:13 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ post '/callback' do
           message = {
             type: 'text',
             text: "beer me"
-          }
+          } 
           response = client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
