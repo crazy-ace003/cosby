1e27f57ab34ef18677d37c29c5721373e3e3bd7c
Michael Meade <crazyace003@gmail.com>
2018-01-29 14:37:06 -0500

    Update web.rb

--- web.rb
+++ web.rb
@@ -38,7 +38,8 @@ post '/callback' do
             response = client.reply_message(event['replyToken'], message)
         end
       end
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+    end
+    when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
