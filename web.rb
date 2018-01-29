4d7cfcd6eedbf87773c85e4bbb0df9c7b2aa8962
Michael Meade <crazyace003@gmail.com>
2018-01-29 14:23:18 -0500

    Update web.rb

--- web.rb
+++ web.rb
@@ -27,7 +27,7 @@ post '/callback' do
         if say_message == "lop"
             message = {
               type: 'text',
-              text: event.message['text']
+              text: 'vag'
             }
             response = client.reply_message(event['replyToken'], message)
         else
