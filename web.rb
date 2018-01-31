7b574c3ee168dc0832e641bbada5c81a9a4bedf7
Michael Meade <crazyace003@gmail.com>
2018-01-30 21:49:58 -0500

    Update web.rb

--- web.rb
+++ web.rb
@@ -41,14 +41,8 @@ post '/callback' do
           }
           client.reply_message(event['replyToken'], message)
         end
-
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
       end
     end
   }
-
   "OK"
 end
